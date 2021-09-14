cb3e860df937a1496c4afd9b339b91be6624426a
GabrielZarate <55642682+GabrielZarate@users.noreply.github.com>
2021-09-13 19:12:56 -0500

    Esfera dando vueltas sobre su eje con una malla encima.

---
+++ JS/index.js
@@ -0,0 +1,46 @@
+ const scene = new THREE.Scene()
+ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+ const renderer = new THREE.WebGLRenderer({ antialias: true})
+ 
+ renderer.setSize( window.innerWidth, window.innerHeight )
+ renderer.setClearColor("#222222")
+ document.body.appendChild( renderer.domElement )
+ camera.position.z = 5
+ 
+ window.addEventListener( 'resize', () => {
+     let width = window.innerWidth
+     let height = window.innerHeight
+     renderer.setSize( width, height )
+     camera.aspect = width / height
+     camera.updateProjectionMatrix()
+ })
+ 
+ var geometry = new THREE.SphereGeometry( 1, 32, 32);
+ var material = new THREE.MeshStandardMaterial( { color: 0xff0051, flatShading: true, metalness: 0, roughness: 1 })
+ var sphere = new THREE.Mesh ( geometry, material )
+ scene.add( sphere )
+ 
+ var geometry = new THREE.SphereGeometry( 3, 64, 64)
+ var material = new THREE.MeshBasicMaterial( {
+     color: "#dadada", wireframe: true, transparent: true
+ })
+ var wireframeSphere = new THREE.Mesh ( geometry, material )
+ scene.add( wireframeSphere )
+ 
+ var ambientLight = new THREE.AmbientLight ( 0xffffff, 0.2)
+ scene.add( ambientLight )
+ 
+ var pointLight = new THREE.PointLight( 0xffffff, 1 );
+ pointLight.position.set( 25, 50, 25 );
+ scene.add( pointLight );
+ 
+ 
+ function animate() {
+     requestAnimationFrame( animate )
+     sphere.rotation.x += 0.01;
+     sphere.rotation.y += 0.01;
+     wireframeSphere.rotation.x -= 0.04;
+     wireframeSphere.rotation.y -= 0.04;
+     renderer.render( scene, camera )
+ }
+ animate()
---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>T03_PPI_Jntroduccion a JS</title>
+        <link rel="stylesheet" href="CSS/css_fuente.css">
+    </head> 
+	<body>
+        <!-- CDN Link to Three.js -->
+            <script src="./JS/three.js"></script>
+            <!--reference your JS file here. Mine looks like below-->
+        <script src="./JS/index.js"></script>
+
+        <script type="text/javascript" src="JS/deploy.js"></script>
+
+        <div class="main-wrapper">
+
+            <h id="P01" class="section-title" onclick="showAnswer('A01')">01.   ¿Qué es WebGL y three.js? (Click en las preguntas.)</h><br>
+            <a id="A01" style="display: none;">
+            <p class="summary box-answer" >WebGL es una API web que permite conectar la aceleración del GPU para renderizar graficos 3d en el navegador.Esto combinado con three.js que es una biblioteca JS, permite la realización de cosas como la mostrada arriba. </p>
+            </a>
+
+        </div>
+
+	</body>
+</html>
